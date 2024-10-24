d7091d1a383fa2a001183af542e1c8ac96f63b00
Ashlin Lee <ashlinlee98@gmail.com>
2024-10-23 23:29:45 -0600

    Completed contact page, started resume page

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -7,72 +7,118 @@ const MyForm = () => {
         message: ''
     });
 
+    const [errors, setErrors] = useState({
+        name: "",
+        email: "",
+        message: "",
+    });
+
+    const [submittedMessage, setSubmittedMessage] = useState('');
+
+    const validateEmail = (email: string) => {
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return regex.test(email);
+    };
+
     const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         setFormData({ ...formData, [e.target.name]: e.target.value });
+        setErrors({ ...errors, [e.target.name]: '' });
     }
+
+    const handleBlur = (e: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const { name, value } = e.target;
+
+        if (!value) {
+            setErrors((prevErrors) => ({ ...prevErrors, [name]: `${name.charAt(0).toUpperCase() + name.slice(1)} is required` }));
+        }
+
+        if (name === 'email' && value && !validateEmail(value)) {
+            setErrors((prevErrors) => ({ ...prevErrors, email: 'Please enter a valid email address' }));
+        }
+    };
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        console.log(formData);
+
+        if (formData.name && formData.email && formData.message) {
+            setSubmittedMessage('Thank you for your message! I will get back to you soon.');
+            setFormData({ name: '', email: '', message: '' });
+        } else {
+            setErrors({
+                name: !formData.name ? 'Name is required' : '',
+                email: !formData.email ? 'Email is required' : '',
+                message: !formData.message ? 'Message is required' : '',
+            });
+
+            setSubmittedMessage('');
+        }
     }
 
     return (
         <div className='bg-blue-300 '>
-            <h2 className="ml-20 text-slate-700 lg:text-5xl text-3xl font-bold px-10 lg:px-16 py-10">Contact Me</h2>
+            <h2 className="ml-20 text-slate-700 lg:text-5xl text-3xl font-bold px-7 md:px-16 py-10">Contact Me</h2>
             <div className='flex justify-center'>
-            <form onSubmit={handleSubmit} className="space-y-4 w-3/4 mb-6">
-                {/* Name Input */}
-                <div>
-                    <label htmlFor="name" className="block font-bold">Name:</label>
-                    <input
-                        type="text"
-                        id="name"
-                        name="name"
-                        value={formData.name}
-                        onChange={handleChange}
-                        className="border rounded-md p-2 w-full"
-                        required
-                    />
-                </div>
+                <form onSubmit={handleSubmit} className="space-y-4 w-3/4 mb-6" data-netlify="true">
+                    <input type="hidden" name="form-name" value="contact" />
+                    {/* Name Input */}
+                    <div>
+                        <label htmlFor="name" className="block font-bold">Name:</label>
+                        <input
+                            type="text"
+                            id="name"
+                            name="name"
+                            value={formData.name}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            className="border rounded-md p-2 w-full"
+                            required
+                        />
+                        {errors.name && <p className="text-red-500 text-xl">{errors.name}</p>}
+                    </div>
 
-                {/* Email Input */}
-                <div>
-                    <label htmlFor="email" className="block font-bold">Email:</label>
-                    <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        value={formData.email}
-                        onChange={handleChange}
-                        className="border rounded-md p-2 w-full"
-                        required
-                    />
-                </div>
+                    {/* Email Input */}
+                    <div>
+                        <label htmlFor="email" className="block font-bold">Email:</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            value={formData.email}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            className="border rounded-md p-2 w-full"
+                            required
+                        />
+                        {errors.email && <p className="text-red-500 text-xl">{errors.email}</p>}
+                    </div>
 
-                {/* Message TextArea */}
-                <div>
-                    <label htmlFor="message" className="block font-bold">Message:</label>
-                    <textarea
-                        id="message"
-                        name="message"
-                        value={formData.message}
-                        onChange={handleChange}
-                        onInput={(e) => {
-                            const target = e.target as HTMLTextAreaElement;
-                            target.style.height = 'auto';
-                            target.style.height = `${target.scrollHeight}px`;
-                        }}
-                        className="border rounded-md h-32 p-2 w-full"
-                        required
-                    />
-                </div>
+                    {/* Message TextArea */}
+                    <div>
+                        <label htmlFor="message" className="block font-bold">Message:</label>
+                        <textarea
+                            id="message"
+                            name="message"
+                            value={formData.message}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            onInput={(e) => {
+                                const target = e.target as HTMLTextAreaElement;
+                                target.style.height = 'auto';
+                                target.style.height = `${target.scrollHeight}px`;
+                            }}
+                            className="border rounded-md h-32 p-2 w-full"
+                            required
+                        />
+                        {errors.message && <p className="text-red-500 text-xl">{errors.message}</p>}
+                    </div>
 
-                {/* Submit Button */}
-                <button type="submit" className="bg-blue-500 text-white p-2 rounded-md">
-                    Submit
-                </button>
-            </form>
+                    {/* Submit Button */}
+                    <button type="submit" className="bg-indigo-400 text-white p-2 rounded-md">
+                        Submit
+                    </button>
+                </form>
             </div>
+            {submittedMessage && <p className="text-black text-center text-xl font-bold p-4">{submittedMessage}</p>}
         </div>
     );
 }
--- src/components/Resume/Resume.tsx
+++ src/components/Resume/Resume.tsx
@@ -1,9 +1,24 @@
 const Resume = () => {
     return (
-        <div className="resume">
-            <h2>Resume</h2>
-            <p>Here is where I will display my resume</p>
-        </div>
+        <div className="bg-blue-300">
+        <h2 className="ml-16 text-slate-700 md:text-5xl text-3xl font-bold md:px-16 py-10">Resume and Skills</h2>
+        <a href="/Documents/example.pdf" download className="text-blue-500 md:text-2xl text-lg underline ml-20">
+            Download my resume
+        </a>
+        <h3 className="md:text-3xl text-xl ml-14 p-6 font-bold">Front-end Proficiencies</h3>
+        <ul className="md:text-2xl md:ml-32 ml-24">
+            <li>Skill 1</li>
+            <li>Skill 2</li>
+            <li>Skill 3</li>
+        </ul>
+
+        <h3 className="md:text-3xl text-xl ml-14 p-6 font-bold">Back-end Proficiencies</h3>
+        <ul className="md:text-2xl md:ml-32 ml-24 pb-6">
+            <li>Skill 1</li>
+            <li>Skill 2</li>
+            <li>Skill 3</li>
+        </ul>
+    </div>
     );
 }
 
