94bf809ae098f8287810d33bbec6c32a9c95c0c0
Ashlin Lee <ashlinlee98@gmail.com>
2024-10-26 16:22:00 -0600

    Finish resume page

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -7,10 +7,15 @@ const About = () => {
                 
                 <img src="./../../../images/Headshot.jpg" alt="professional headshot" className="w-96 rounded-lg lg:border-solid lg:border-8 lg:border-yellow-500 lg:ml-4" />
                 
-                <p className="bg-indigo-300 text-center text-black text-lg lg:text-2xl px-9 py-5 lg:border-solid lg:border-8 lg:border-yellow-500 rounded-lg lg:ml-20 lg:mr-10 lg:mb-20">
-                    I'm a full stack developer with a passion for creating beautiful and functional web applications. I have experience with HTML, CSS, JavaScript, React, Node.js, Express, and MongoDB. I'm always looking for new opportunities to learn and grow as a developer.
-                </p>
-            
+                <div className="bg-indigo-300 text-center text-black text-lg lg:text-2xl px-9 py-5 lg:border-solid lg:border-8 lg:border-yellow-500 rounded-lg lg:ml-20 lg:mr-10 lg:mb-20">
+                    <p>
+                        I'm a recent bootcamp graduate and full stack developer. I love coding because it allows me to work in logic and problem solving while creating something beautiful and deeply useful to other people. I have experience with HTML, CSS, JavaScript, TypeScript, React, Node.js, Express, Python, PostgreSQL and MongoDB. I'm always looking for new opportunities to learn and grow as a developer.
+                    </p>
+                    <br />
+                    <p>
+                        Some fun things about me are that I'm from Utah, have a degree in Philosophy from Brigham Young University, and speak Spanish fluently. When I'm not coding I enjoy reading, overanalyzing movies, and spending time with friends and family.
+                    </p>
+                </div>
             </div>
         </section>
     );
--- src/components/Resume/Resume.tsx
+++ src/components/Resume/Resume.tsx
@@ -2,21 +2,27 @@ const Resume = () => {
     return (
         <div className="bg-blue-300">
         <h2 className="ml-16 text-slate-700 md:text-5xl text-3xl font-bold md:px-16 py-10">Resume and Skills</h2>
-        <a href="/Documents/example.pdf" download className="text-blue-500 md:text-2xl text-lg underline ml-20">
+        <a href="/Documents/ashlin_lee_resume.docx.pdf" download className="text-blue-500 md:text-2xl text-lg underline ml-20">
             Download my resume
         </a>
         <h3 className="md:text-3xl text-xl ml-14 p-6 font-bold">Front-end Proficiencies</h3>
         <ul className="md:text-2xl md:ml-32 ml-24">
-            <li>Skill 1</li>
-            <li>Skill 2</li>
-            <li>Skill 3</li>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>TypeScript</li>
+            <li>React</li>
+            <li>RESTful APIs</li>
         </ul>
 
         <h3 className="md:text-3xl text-xl ml-14 p-6 font-bold">Back-end Proficiencies</h3>
         <ul className="md:text-2xl md:ml-32 ml-24 pb-6">
-            <li>Skill 1</li>
-            <li>Skill 2</li>
-            <li>Skill 3</li>
+            <li>Node.js</li>
+            <li>Express.js</li>
+            <li>PostreSQL</li>
+            <li>MongoDB</li>
+            <li>API integration</li>
+            <li>Authentication</li>
         </ul>
     </div>
     );
