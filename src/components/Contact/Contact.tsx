647721e76f179a24bff9654c1294801530a92937
Ashlin Lee <ashlinlee98@gmail.com>
2025-04-04 00:36:49 -0600

    Fixed bug with contact section

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,48 +1,4 @@
-import React, { useState } from 'react';
-
-const MyForm = () => {
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        message: ''
-    });
-
-    const [errors, setErrors] = useState({
-        name: "",
-        email: "",
-        message: "",
-    });
-
-    const [submittedMessage, setSubmittedMessage] = useState('');
-
-    const validateEmail = (email: string) => {
-        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return regex.test(email);
-    };
-
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value });
-        setErrors({ ...errors, [e.target.name]: '' });
-    }
-
-    const handleBlur = (e: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        const { name, value } = e.target;
-
-        if (!value) {
-            setErrors((prevErrors) => ({ ...prevErrors, [name]: `${name.charAt(0).toUpperCase() + name.slice(1)} is required` }));
-        }
-
-        if (name === 'email' && value && !validateEmail(value)) {
-            setErrors((prevErrors) => ({ ...prevErrors, email: 'Please enter a valid email address' }));
-        }
-    };
-
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault(); // Prevent the default form submission
-        setSubmittedMessage("Thanks for reaching out! I'll get back to you soon.");
-        setFormData({ name: '', email: '', message: '' });
-    };
-
+const Contact = () => {
     return (
         <section id="contact">
             <div>
@@ -53,4 +9,4 @@ const MyForm = () => {
     );
 }
 
-export default MyForm;
+export default Contact;
