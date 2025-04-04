de4f181949687a0053539e455a4850fc2a989ab9
Ashlin Lee <ashlinlee98@gmail.com>
2025-04-03 23:07:50 -0600

    Updated favicon

--- src/components/Resume/Resume.tsx
+++ src/components/Resume/Resume.tsx
@@ -3,7 +3,7 @@ const Resume = () => {
         <section id="resume">
             <div>
         <h2 className="ml-16 text-slate-700 md:text-5xl text-3xl font-bold md:px-16 py-10">Resume and Skills</h2>
-        <a href="/Documents/ashlin_lee_resume.docx.pdf" download className="text-blue-500 md:text-2xl text-lg underline ml-20">
+        <a href="/Documents/ashlin_lee_resume.docx.pdf" download className="text-cyan-500 md:text-2xl text-lg underline ml-20">
             Download my resume
         </a>
         <h3 className="md:text-3xl text-xl ml-14 p-6 font-bold">Front-end Proficiencies</h3>
